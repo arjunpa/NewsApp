d6fc91b9734cd7bfe8210b2091e97b124f29c484
Arjun P A <arjun.pa2@gmail.com>
2020-08-20 09:08:14 +0530

    Implement DiskCache for persisting article list

--- NewsApp/ArticleListRepositoryInterface.swift
+++ NewsApp/ArticleListRepositoryInterface.swift
@@ -17,8 +17,11 @@ final class ArticleListRepository: ArticleListRepositoryInterface {
     
     private let apiService: APIServiceInterface
     
-    init(with apiService: APIServiceInterface) {
+    private let diskCache: URLCacheable
+    
+    init(with apiService: APIServiceInterface, diskCache: URLCacheable = DiskURLCache.default) {
         self.apiService = apiService
+        self.diskCache = diskCache
     }
     
     func fetchArticles(completion: @escaping (Result<ArticleListResponse, Error>) -> Void) {
@@ -28,12 +31,31 @@ final class ArticleListRepository: ArticleListRepositoryInterface {
                               parameters: nil,
                               headers: nil,
                               encoding: GetRequestEncoding())
+        let cacheableRequest = CacheableRequest(request: request,
+                                                expiry: .aged(86400))
         
-        self.apiService.request(for: request) { (result: Result<APIHTTPDecodableResponse<ArticleListResponse>, Error>) in
+        self.apiService.request(for: request) {[weak self] (result: Result<APIHTTPDecodableResponse<ArticleListResponse>, Error>) in
             switch result {
             case .success(let response):
                 completion(.success(response.decoded))
             case .failure(let error):
+                self?.handleFailureIfRequired(forRequest: cacheableRequest, error: error, completion: completion)
+            }
+        }
+    }
+    
+    private func handleFailureIfRequired(forRequest request: CacheableRequest,
+                                         error: Error,
+                                         completion: @escaping (Result<ArticleListResponse, Error>) -> Void) {
+        self.diskCache.get(forRequest: request) { (result: Result<APIHTTPDecodableResponse<ArticleListResponse>?, Error>) in
+            switch result {
+            case .success(let response):
+                guard let response = response else {
+                    completion(.failure(error))
+                    return
+                }
+                completion(.success(response.decoded))
+            case .failure:
                 completion(.failure(error))
             }
         }
---
+++ NewsApp/URLCacheable.swift
@@ -0,0 +1,16 @@
+//
+//  URLCacheable.swift
+//  PhotoCollection
+//
+//  Created by Arjun P A on 19/08/20.
+//  Copyright © 2020 Arjun P A. All rights reserved.
+//
+
+import Foundation
+
+typealias URLCacheableStoreCompletion = (Result<Bool, Error>) -> ()
+
+protocol URLCacheable {
+    func store<T: Decodable>(response: APIHTTPDecodableResponse<T>, forRequest request: CacheRequestable, completion: URLCacheableStoreCompletion?)
+    func get<T: Decodable>(forRequest request: CacheRequestable, completion: @escaping (Result<APIHTTPDecodableResponse<T>?, Error>) -> Void)
+}
